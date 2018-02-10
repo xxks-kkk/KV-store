8fca60bf1dc29429fc1b02fc17715fea1a4defe0
Jianwei Chen <jianweichen@utexas.edu>
2018-02-10 11:05:14 -0600

    format files

--- watchdog.py
+++ watchdog.py
@@ -6,25 +6,30 @@ import subprocess
 import sys
 import config
 log.startLogging(config.LOG_FILE)
+
+
 class WatchDogServer(xmlrpc.XMLRPC):
     proc = None
+
     def xmlrpc_joinserver(self):
         if self.proc is not None:
             return xmlrpc.Fault(1, "Server already started.")
         try:
             self.proc = subprocess.Popen(["python2", "server.py"])
-            return 0 
+            return 0
         except Exception as e:
             return xmlrpc.Fault(2, str(e))
+
     def xmlrpc_killserver(self):
         if self.proc is None:
             return xmlrpc.Fault(1, "Server hasn't started.")
         try:
             self.proc.kill()
             self.proc = None
-            return 0 
+            return 0
         except Exception as e:
             return xmlrpc.Fault(2, str(e))
+
 
 if __name__ == '__main__':
     from twisted.internet import reactor
@@ -33,8 +38,5 @@ if __name__ == '__main__':
     watchdog = WatchDogServer()
     port = config.WATCHDOG_PORT
     reactor.listenTCP(port, server.Site(watchdog))
-    log.msg("WatchDog Server Running on {}.".format(port)) 
+    log.msg("WatchDog Server Running on {}.".format(port))
     reactor.run()
-
-    
-
