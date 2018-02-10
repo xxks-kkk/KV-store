f4ba6ffe22bf299669cbd450520ccfacdce23d62
sunwell1994 <divinerage1994@gmail.com>
2018-02-10 10:51:37 -0600

    simple vector clock
    simple vector clock

--- clock.py
+++ clock.py
@@ -1,14 +1,27 @@
 # Purpose: vector clock
 
 def isHappenBefore(c1, c2):
-    pass
+    for i in range(c1.server_num):
+        if c1.vector_clock[i] > c2.vector_clock[i]:
+            c2.vector_clock[i] = c1.vector_clock[i] 
+        else:
+            c1.vector_clock[i] = c2.vector_clock[i] 
+
 
 class Clock:
+    # the numbers of server in the system
+    server_num = 5
+    vector_clock = []
+    
     def __init__(self):
-        pass
+        self.vector_clock = [0] * self.server_num
+
     
     def incrementClock(self, id):
-        pass
+        self.vector_clock[id] += 1
+
 
     def onMessageReceived(self, id, c1):
-        pass
+        self.incrementClock(id)
+        isHappenBefore(self, c1)
+
