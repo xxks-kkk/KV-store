c4450116d7093414bc983d1ba3abddfb84a8fc4b
Jianwei Chen <jianweichen@utexas.edu>
2018-02-14 18:19:24 -0600

    Add specification and comments

--- model.py
+++ model.py
@@ -2,17 +2,41 @@
 # (handling put/get requests)
 
 class Model:
+
+    writeLog = None
+    """
+    [
+    "UUID": ["key", "value", "timeStamp", "serverId", "receiptVector"],
+    "UUID": ["key", "value", "timeStamp", "serverId", "receiptVector"],
+    ]
+    """
     def __init__(self, server):
+        self.server = server
         pass
 
-    def onStablize(self):
-        pass
+    # def onStablize(self):
+    #     pass
 
     def onPrintStore(self):
+        # return the dictionary content to a string
         pass
 
-    def put(self, key, value, timeStamp):
+    def put_internal(self, item):
+        """
+        item as a dictionary:
+        "key", "value", "serverId", "timeStamp"
+        """
+        pass
+
+    def put(self, item):
+        """
+        item as a dictionary:
+        "key", "value", "serverId", "timeStamp"
+        """
         pass
 
     def get(self, key):
+        """
+        return according to API specification.
+        """
         pass
