5479976647445ef566044633d623968b5839267f
sunwell1994 <divinerage1994@gmail.com>
2018-02-17 14:45:27 -0600

    the bugs for the config call
    the bugs for the config call

--- master.py
+++ master.py
@@ -9,24 +9,24 @@ def killServer(dogs, clients, servers, arg):
     dogs[int(arg[1])].killServer()
 
 def joinClient(dogs, clients, servers, arg):
-    clients[int(arg[1]) % CLIENT_COUNT].joinServer(int(arg[2]))
+    clients[int(arg[1]) % config.CLIENT_COUNT].joinServer(int(arg[2]))
     pass
 
 def breakConnection(dogs, clients, servers, arg):
     # if break between client and server, send to client, 
     # elseif between 2 servers, send to first server to execute
     id1, id2 = int(arg[1]), int(arg[2])
-    if id1 < SERVER_COUNT and id2 < SERVER_COUNT:
+    if id1 < config.SERVER_COUNT and id2 < config.SERVER_COUNT:
         servers[id1].breakConnection(id2)
     else:
-        clients[max(id1, id2) % CLIENT_COUNT].breakConnection(min(id1, id2))
+        clients[max(id1, id2) % config.CLIENT_COUNT].breakConnection(min(id1, id2))
 
 def createConnection(dogs, clients, servers, arg):
     id1, id2 = int(arg[1]), int(arg[2])
-    if id1 < SERVER_COUNT and id2 < SERVER_COUNT:
+    if id1 < config.SERVER_COUNT and id2 < config.SERVER_COUNT:
         servers[id1].createConnection(id2)
     else:
-        clients[max(id1, id2) % CLIENT_COUNT].createConnection(min(id1, id2))
+        clients[max(id1, id2) % config.CLIENT_COUNT].createConnection(min(id1, id2))
 
 def stablize(dogs, clients, servers, arg):
     for server in servers:
@@ -37,20 +37,20 @@ def printStore(dogs, clients, servers, arg):
 
 def put(dogs, clients, servers, arg):
     # what happens if there are ' ' in key and value
-    clients[int(arg[1]) % CLIENT_COUNT].put(arg[2], arg[3])
+    clients[int(arg[1]) % config.CLIENT_COUNT].put(arg[2], arg[3])
 
 def get(dogs, clients, servers, arg):
-    clients[int(arg[1]) % CLIENT_COUNT].get(arg[2])
+    clients[int(arg[1]) % config.CLIENT_COUNT].get(arg[2])
 
 if __name__ == "__main__":
     # 1. connect with five server watchdogs and five clients and five servers
     #    (NOTE: connect with watchdogs before servers)
     # 2. Wait for the Samantha's command
     dogs, clients, servers = [], [], []
-    for i in range(SERVER_COUNT):
-        dogs.append( xmlrpclib.ServerProxy('http://' + WATCHDOG_IP_LIST[i] + ':' + WATCHDOG_PORT[i]))
-        clients.append(xmlrpclib.ServerProxy('http://' + CLIENT_IP_LIST[i]+ ':' + CLIENT_PORT[i]))
-        servers.append(xmlrpclib.ServerProxy('http://' + WATCHDOG_IP_LIST[i] + ':' + SERVER_PORT[i]))
+    for i in range(config.SERVER_COUNT):
+        dogs.append( xmlrpclib.ServerProxy('http://' + config.WATCHDOG_IP_LIST[i] + ':' + config.WATCHDOG_PORT[i]))
+        clients.append(xmlrpclib.ServerProxy('http://' + config.CLIENT_IP_LIST[i]+ ':' + config.CLIENT_PORT[i]))
+        servers.append(xmlrpclib.ServerProxy('http://' + config.WATCHDOG_IP_LIST[i] + ':' + config.SERVER_PORT[i]))
 
     command2func = {
                     'joinServer' : joinServer,
--- router.py
+++ router.py
@@ -1,11 +1,12 @@
 import config
+import random
+
 class Router:
-    
     def __init__(self, serverId):
         # neighbours = [0] * SERVER_COUNT
         # neighbours[serverId] = 1
         self.id = serverId
-        self.routeMat = [[]] * SERVER_COUNT
+        self.routeMat = [[]] * config.SERVER_COUNT
         self.routeMat[serverId] = [serverId]
 
     def receivedPayload(self, payload):
@@ -15,12 +16,16 @@ class Router:
         '''
         sendId = payload[0]
         sendRouteMat = payload[1]
+        isChange = False
         #Build new connection for forward
-        for i in range(SERVER_COUNT):
+        for i in range(config.SERVER_COUNT):
             if len(self.routeMat[i]) == 0 and sendRouteMat[i]:
                 self.routeMat[i] = [sendId] + sendRouteMat[i]
+                isChange = True
             if self.routeMat[i] and self.routeMat[i][0] == sendId and len(sendRouteMat[i]) == 0:
                 self.routeMat[i] = []
+                isChange = True
+        return isChange
 
     def getPayload(self):
         return (self.id , self.routeMat)
@@ -29,7 +34,7 @@ class Router:
         if len(self.routeMat[receiveId]):
             return self.routeMat[receiveId][0]
         else:
-            return SERVER_COUNT
+            return False
 
     def neighbourChange(self, serverId, val):
         '''
@@ -43,8 +48,34 @@ class Router:
         else:
             self.routeMat[serverId] = []
 
-    if __name__ == '__main__':
-        pass
+    def showRouters(self):
+        print self.routeMat
+
+if __name__ == '__main__':
+    routers = []
+    # all are neighbours at first
+    for i in range(config.SERVER_COUNT):
+        routers.append(Router(i))
+        for j in range(config.SERVER_COUNT):
+            routers[i].neighbourChange(j, True)
+
+    # 0 3  ==> break
+    routers[0].neighbourChange(3, False)
+    routers[3].neighbourChange(0, False)
+    routers[2].neighbourChange(3, False)
+    routers[3].neighbourChange(2, False)
+    routers[1].neighbourChange(3, False)
+    routers[3].neighbourChange(1, False)
+
+    routers[0].showRouters()
+    routers[0].receivedPayload(routers[2].getPayload())
+    routers[3].receivedPayload(routers[4].getPayload())
+
+    #
+
+    routers[0].showRouters()
+    routers[3].showRouters()
+
 
 
         
