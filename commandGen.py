6b2a525b3f94910b5853f16d92f78b969150673b
sunwell1994 <divinerage1994@gmail.com>
2018-02-27 16:26:32 -0600

    test case

--- commandGen.py
+++ commandGen.py
@@ -21,7 +21,14 @@ clientid = list('56789')
 chars = '1234567890-=qwertyuiopasdfghjklzxcvbnm,.//]!@#$%^&*())'
 lenchars = len(chars)
 
-with open('commandList', 'w') as f:
+
+
+
+with open('commandConnected', 'w') as f:
+    for i in range(5):
+        f.write("joinServer %d\n" % (i))
+        f.write("joinClient %d %d\n" % (i + 5, i))
+
     for _ in range(command_Nums):
         length = random.randint(600,1000)
         k = key[random.randint(0, 6)]
@@ -30,3 +37,10 @@ with open('commandList', 'w') as f:
             c += chars[random.randint(0, lenchars - 1)]
 
         f.write('put ' + clientid[random.randint(0,4)] + ' ' + k + ' ' + c + '\n')
+
+    f.write("stablize\n")
+
+    for i in range(5):
+        f.write("printStore %d\n" % (i))
+        
+    f.write("#\n")
